9b744b3112a12444035fe88d696d2ca3ed40b8b0
flip111 <flip101@gmail.com>
2024-11-24 20:44:31 +0100

    Fix App.hs

--- src/GHC/Compiler/Notes/App.hs
+++ src/GHC/Compiler/Notes/App.hs
@@ -1,62 +1,61 @@
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TypeApplications #-}
 
-module GHC.Compiler.Notes.App where
+module GHC.Compiler.Notes.App
+  ( AppContext(..)
+  , AppContextCommon(..)
+  , OnOff
+  , AppT(..)
+  , ghcInitSession
+  , appContext
+  , defaultAppContext
+  ) where
 
-import           Capability.Accessors
-import           Capability.Reader
-
-import           Control.Monad
-import           Control.Monad.Catch
-
-import           Control.Monad.IO.Class
-import           Control.Monad.Trans.Reader (ReaderT(..))
-
-import           Control.Monad.Trans.State
-
-import           Data.Kind
-
-import qualified Data.Text                  as Text
-
+import Capability.Accessors
+import Capability.Reader
+import Capability.Source (HasSource(..))
+import Control.Monad
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader (ReaderT(..))
+import Control.Monad.Trans.State
+import Data.Kind
+import qualified Data.Text as Text
 import qualified GHC.Driver.Flags as Flags
 import qualified GHC.Driver.Session as Session
-import qualified GHC.Driver.Monad as GHCMonad
-
-import           GHC                        (getSessionDynFlags, runGhc)
-
-import           GHC.Compiler.Notes.Types
-import           GHC.Generics
-
-import           GHC.LanguageExtensions     (Extension)
+import GHC (getSessionDynFlags, runGhc)
+import GHC.Generics
+import GHC.LanguageExtensions (Extension)
 import qualified GHC.Paths
-
 import GHC.Types.SrcLoc
+import GHC.Compiler.Notes.Types
+import Prelude
 
-data AppContext = AppContext { envSession :: Session.DynFlags
-                             , envSourceResourceGetter :: FilePath -> Maybe SrcSpan -> Text.Text
-                             }
-  deriving Generic
+data AppContext = AppContext
+  { envSession :: Session.DynFlags
+  , envSourceResourceGetter :: FilePath -> Maybe SrcSpan -> Text.Text
+  }
+  deriving stock Generic
 
 type OnOff a = (Bool, a)
 
-data AppContextCommon = AppContextCommon { sFlagsExtensions         :: [OnOff Extension]
-                                         , sFlagsGeneralOptions     :: [OnOff Flags.GeneralFlag]
-                                         , sFlagsGlobalIncludePaths :: [String]
-                                         }
+data AppContextCommon = AppContextCommon
+  { sFlagsExtensions :: [OnOff Extension]
+  , sFlagsGeneralOptions :: [OnOff Flags.GeneralFlag]
+  , sFlagsGlobalIncludePaths :: [String]
+  }
 
 ghcInitSession :: MonadIO m => m Session.DynFlags
 ghcInitSession = liftIO $ runGhc (Just GHC.Paths.libdir) getSessionDynFlags
-
 
 appContext :: MonadIO m => AppContextCommon -> m AppContext
 appContext AppContextCommon{..} = do
   defDflags <- ghcInitSession
   let dflags = execState dflagsUpdater defDflags
-
-  return $ AppContext { envSession = dflags, envSourceResourceGetter = gitlabResourceGetter }
+  return $ AppContext
+    { envSession = dflags
+    , envSourceResourceGetter = gitlabResourceGetter
+    }
   where
     dflagsUpdater = do
       forM_ sFlagsGeneralOptions $ \(b, opt) -> modify'
@@ -77,14 +76,18 @@ appContext AppContextCommon{..} = do
 
 defaultAppContext :: MonadIO m => m AppContext
 defaultAppContext = appContext $
-  AppContextCommon { sFlagsExtensions         = []
-                   , sFlagsGeneralOptions     =
-                       [(True, Flags.Opt_Haddock), (True, Flags.Opt_KeepRawTokenStream)]
-                   , sFlagsGlobalIncludePaths = [ "dummy_includes"
-                                                , "output/ghc/compiler"
-                                                , "output/ghc/libraries/base/include"
-                                                ]
-                   }
+  AppContextCommon
+    { sFlagsExtensions = []
+    , sFlagsGeneralOptions =
+        [ (True, Flags.Opt_Haddock)
+        , (True, Flags.Opt_KeepRawTokenStream)
+        ]
+    , sFlagsGlobalIncludePaths =
+        [ "dummy_includes"
+        , "output/ghc/compiler"
+        , "output/ghc/libraries/base/include"
+        ]
+    }
 
 newtype AppT (m :: Type -> Type) a = AppT { runAppT :: AppContext -> m a }
   deriving
@@ -95,9 +98,17 @@ newtype AppT (m :: Type -> Type) a = AppT { runAppT :: AppContext -> m a }
     , MonadThrow
     ) via (ReaderT AppContext m)
 
+instance Monad m => HasSource "AppContext" AppContext (AppT m) where
+  await_ _ = AppT (\ctx -> pure ctx)
+  {-# INLINE await_ #-}
+
 deriving
   via (MonadReader (ReaderT AppContext m))
   instance Monad m => HasReader "AppContext" AppContext (AppT m)
+
+instance Monad m => HasSource "envSession" Session.DynFlags (AppT m) where
+  await_ _ = AppT (\ctx -> pure (envSession ctx))
+  {-# INLINE await_ #-}
 
 deriving
   via (Field "envSession" "ctx" (MonadReader (ReaderT AppContext m)))
